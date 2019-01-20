546d4cbe73c36ce404139fc4fd8caf298502e85f
SimasDei <simondeiman@gmail.com>
2019-01-20 23:59:36 +0200

    add body parser

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const router = express.Router();
 
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/add-product', (req, res, next) => {
+router.use('/add-product', (req, res, next) => {
   res.send(
     '<form action="/product" method="POST" > <input type="text" name="title" /> <button type="submit">Submit</button> </form>'
   );
 });
 
-app.post('/product', (req, res, next) => {
+router.post('/product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
 
-app.listen(3000);
+module.exports = router;
