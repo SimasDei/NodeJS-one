0149bd7e42625d4b4cb7503abaf88f586077583e
SimasDei <simondeiman@gmail.com>
2019-01-21 00:30:34 +0200

    serve html pages using sendFile method, import global path

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST" > <input type="text" name="title" /> <button type="submit">Submit</button> </form>'
-  );
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>No no... Mr.Griffin no home 404.</h1>');
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,12 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
-router.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST" > <input type="text" name="title" /> <button type="submit">Submit</button> </form>'
-  );
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
