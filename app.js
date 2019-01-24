ea4a69b8ae0f3c820602c2f0956dd13a49fc8ac0
SimasDei <simondeiman@gmail.com>
2019-01-24 18:04:53 +0200

    add templating engines

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
-
 const rootDir = require('../util/path');
 
+const adminData = require('./admin');
+
 router.get('/', (req, res, next) => {
+  console.log(adminData.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
