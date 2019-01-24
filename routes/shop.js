5c0373ec48e176711003315c8ec9ac4046aaf6d0
SimasDei <simondeiman@gmail.com>
2019-01-24 18:24:23 +0200

    use pug template engine

--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,8 @@ const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', { prods: products, docTitle: 'Shop' });
 });
 
 module.exports = router;
